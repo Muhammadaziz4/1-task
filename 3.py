859a309a5de8b5a6753c845db737a0aa773c8f90
Muhammadaziz Usmonjonov <usmonjonovmuhammadaziz63@gmail.com>
2021-11-10 05:08:26 -0500

    first commit

---
+++ 3.py
@@ -0,0 +1,6 @@
+num = [1,2,3,4,5]
+def findandgiveAnameforevenAndoddN(num):
+        if num % 2 == 0: print(['1'])
+        if num % 2 !=0: print(['5']) 
+        return num
+print(findandgiveAnameforevenAndoddN(num))
